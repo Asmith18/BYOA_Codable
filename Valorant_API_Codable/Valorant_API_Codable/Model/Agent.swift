32f5b16855412d686cf9aecb42c39ba5091e4c79
Asmith18 <Smittysadam@gmail.com>
2022-02-09 12:01:14 -0600

    storyboard and model done i think

---
+++ Valorant_API_Codable/Valorant_API_Codable/Model/Agent.swift
@@ -0,0 +1,30 @@
+//
+//  Agent.swift
+//  Valorant_API_Codable
+//
+//  Created by adam smith on 2/9/22.
+//
+
+import Foundation
+
+struct TopLevelDict: Decodable {
+    let data: [AgentData]
+}
+
+struct AgentData: Decodable {
+    let displayName: String
+    let displayIcon: String
+    let role: AgentRole
+    let abilities: [AgentAbilities]
+    let description: String
+}
+
+struct AgentRole: Decodable {
+    let displayName: String
+    let displayIcon: String
+}
+
+struct AgentAbilities: Decodable {
+    let displayName: String
+    let displayIcon: String
+}
